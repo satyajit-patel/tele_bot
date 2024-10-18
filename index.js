86cf671ed0cbc4c95a148bf3ba4f7a2138b90e2c
satyajitPatel <thriver1988@gmail.com>
2024-10-19 01:11:39 +0530

    joke bot is up

---
+++ index.js
@@ -0,0 +1,55 @@
+/*
+    npm init -y
+    npm i node-telegram-bot-api
+    get api key from telegram => BotFather -> /newbot -> give [botName] and [userName]
+    secure your api key => npm i dotenv
+    make n/w calls => npm i axios
+    start server => node index.js
+*/
+
+const dotenv = require('dotenv');
+dotenv.config();
+const axios = require('axios');
+
+const telegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.apiKey;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new telegramBot(token, {polling: true});
+
+bot.onText(/\/joke/, async (msg) => {
+    const chatId = msg.chat.id;
+    const response = await axios.get(process.env.apiCall);
+    /*
+                     _______________
+        structure of |response.data|
+                     _______________
+        {
+            "type": "programming",
+            "setup": "I was gonna tell you a joke about UDP...",
+            "punchline": "...but you might not get it.",
+            "id": 71
+        }
+    */
+    const setup = response.data.setup;
+    const punchline = response.data.punchline;
+  
+    // console.log(response.data);
+    bot.sendMessage(chatId, setup + ', ' + punchline);
+});
+
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+// bot.on('message', (msg) => {
+//     // 'msg' is the received Message from Telegram
+//     const chatId = msg.chat.id;
+//     const chatName = msg.chat.first_name;
+  
+//     // send a message to the chat acknowledging receipt of their message
+//     bot.sendMessage(chatId, ` Hi ${chatName} please type the secret word to get specific replay. What was the WORD`);
+// });
+
+console.log('app is running');
